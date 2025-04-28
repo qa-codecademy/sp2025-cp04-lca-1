0d92588042dab1e10b65b8399115e07a98b615cf
Filip Trifunovski <filiptrifunovski88@gmail.com>
2025-04-28 23:45:39 +0200

    update01

--- headerscript.js
+++ headerscript.js
@@ -1,4 +1,27 @@
 document.getElementById("menu-icon").addEventListener("click", function () {
   const menu = document.getElementById("menu");
   menu.classList.toggle("active");
+
+  const bars = document.querySelectorAll("#menu-icon .bar");
+  bars.forEach((bar, index) => {
+    bar.classList.toggle(`bar-${index + 1}-active`);
+  });
+});
+
+const menuLinks = document.querySelectorAll(
+  ".header-nav-item a, .header-language img"
+);
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    const menu = document.getElementById("menu");
+    const bars = document.querySelectorAll("#menu-icon .bar");
+
+    if (menu.classList.contains("active")) {
+      menu.classList.remove("active");
+      bars.forEach((bar, index) => {
+        bar.classList.remove(`bar-${index + 1}-active`);
+      });
+    }
+  });
 });
